b21a0ddcac601090d51fb479d84df64986d06c5c
Adam El Taha <94598096+incogiscool@users.noreply.github.com>
2026-02-07 18:58:41 -0500

    feat: Omak Page

---
+++ app/omak/page.tsx
@@ -0,0 +1,11 @@
+export default function Omak() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex w-full flex-col items-center px-4 sm:px-6 lg:px-8">
+        <h1 className="text-4xl font-bold tracking-tight text-black dark:text-zinc-50 sm:text-5xl">
+          omak
+        </h1>
+      </main>
+    </div>
+  );
+}
